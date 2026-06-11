b1c3b448fedfaf3b0d26e4d172a210fad2b90b5e
Hallel <hallelkedar000@gmail.com>
2026-06-11 09:50:06 +0300

    Added MySQL connection and table creation function

---
+++ database/db_connection.py
@@ -0,0 +1,48 @@
+import mysql.connector
+from secret import DB_PASSWORD
+
+def get_connect(host, user, password, database):
+    conn = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    
+    return conn
+
+
+def create_tables():
+
+    conn = get_connect(host='localhost',
+                    user='root', 
+                    password=DB_PASSWORD,
+                    database='library_db'
+                    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS books (
+                    id INT AUTO_INCERMENT PRIMARY KEY,
+                    title VARCHAR(50) NOT NULL,
+                    author VARCHAR(50) NOT NULL,
+                    genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other'),
+                    is_available BOOLEAN NOT NULL,
+                    borrowed_by_member_id INT UNIQUE
+                    );
+            ''')
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS members (
+                    id INT AUTO_INCERMENT PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL,
+                    email VARCHAR(255) UNIQUE NOT NULL,
+                    is_active BOOLEAN NOT NULL,
+                    total_borrows INT AUTO_INCREMENT NOT NULL
+                   );
+                   ''')
+    conn.commit()
+
+def close_db(cursor, conn):
+    cursor.close()
+    conn.close()
+
