e5ac430519bd2c9433e9f1a5f06efdf114f2b9cc
Hallel <hallelkedar000@gmail.com>
2026-06-11 20:15:38 +0300

    Update get_connection in every method - to ensure it's connect

--- database/basemodel_db.py
+++ database/basemodel_db.py
@@ -4,19 +4,19 @@ from secret import DB_PASSWORD
 class BaseModel:
     def __init__(self, table_name):
         self.table_name = table_name
-        self.conn = db_connection.get_connection()
+        self.db = db_connection
 
     def get_all_items(self) -> list[dict]:
-        
-        with self.conn.cursor(dictionary=True) as cursor:
+        conn = self.db.get_connection()
+        with conn.cursor(dictionary=True) as cursor:
             cursor.execute(f'SELECT * FROM {self.table_name}')
             
             books = cursor.fetchall()
             return books
     
     def get_item_by_id(self, message_id: int) -> dict | None:
-        with self.conn.cursor(dictionary=True) as cursor:
-
+        conn = self.db.get_connection()
+        with conn.cursor(dictionary=True) as cursor:
             query = f'SELECT * FROM {self.table_name} WHERE id = %s'
             cursor.execute(query, (message_id,))
 
@@ -24,7 +24,8 @@ class BaseModel:
             return item
     
     def create_item(self, data: dict) -> int:
-        with self.conn.cursor(dictionary=True) as cursor:
+        conn = self.db.get_connection()
+        with conn.cursor(dictionary=True) as cursor:
             columns = ', '.join(data)
             placeholders = ', '.join(['%s'] * len(data))
 
@@ -38,7 +39,8 @@ class BaseModel:
             return new_id
     
     def update_item(self, item_id: int, data: dict) -> bool:
-        with self.conn.cursor(dictionary=True) as cursor:
+        conn = self.db.get_connection()
+        with conn.cursor(dictionary=True) as cursor:
             set_clause = [f'{key} = %s' for key in data]
 
             query = f'UPDATE {self.table_name} SET ({set_clause}) WHERE id = %s'
@@ -51,7 +53,8 @@ class BaseModel:
             return changed
     
     def delete_item(self, item_id: int) -> bool:
-        with self.conn.cursor(dictionary=True) as cursor:
+        conn = self.db.get_connection()
+        with conn.cursor(dictionary=True) as cursor:
             query = f'DELETE FROM {self.table_name} WHERE id = %s'
             
             cursor.execute(query, (item_id,))
