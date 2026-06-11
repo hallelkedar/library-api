1535752a0d54ef8b2899a6dbaccce3030565a6a4
Hallel <hallelkedar000@gmail.com>
2026-06-11 14:43:02 +0300

    Added connection from db

--- database/basemodel_db.py
+++ database/basemodel_db.py
@@ -7,59 +7,55 @@ class BaseModel:
         self.conn = db_connection.get_connection()
 
     def get_all_items(self) -> list[dict]:
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM %s', (self.table_name,))
         
-        books = cursor.fetchall()
-        cursor.close()
-        return books
+        with self.conn.cursor(dictionary=True) as cursor:
+            cursor.execute(f'SELECT * FROM {self.table_name}')
+            
+            books = cursor.fetchall()
+            return books
     
     def get_item_by_id(self, message_id: int) -> dict | None:
-        cursor = self.conn.cursor()
+        with self.conn.cursor(dictionary=True) as cursor:
 
-        query = f'SELECT * FROM {self.table_name} WHERE id = %s'
-        cursor.execute(query, (message_id,))
+            query = f'SELECT * FROM {self.table_name} WHERE id = %s'
+            cursor.execute(query, (message_id,))
 
-        item = self.cursor.fetchone()
-        cursor.close()
-        return item
+            item = self.cursor.fetchone()
+            return item
     
     def create_item(self, data: dict) -> int:
-        cursor = self.conn.cursor()
-        columns = ', '.join(data)
-        placeholders = ', '.join(['%s'] * len(data))
+        with self.conn.cursor(dictionary=True) as cursor:
+            columns = ', '.join(data)
+            placeholders = ', '.join(['%s'] * len(data))
 
-        query = f'INSERT INTO {self.table_name} ({columns} VALUES ({placeholders}))'
-        params = tuple(data.values())
+            query = f'INSERT INTO {self.table_name} ({columns} VALUES ({placeholders}))'
+            params = tuple(data.values())
 
-        cursor.execute(query, params)
-        self.conn.commit()
+            cursor.execute(query, params)
+            self.conn.commit()
 
-        new_id = cursor.lastrowid
-        cursor.close()
-        return new_id
+            new_id = cursor.lastrowid
+            return new_id
     
     def update_item(self, item_id: int, data: dict) -> bool:
-        cursor = self.conn.cursor()
-        set_clause = [f'{key} = %s' for key in data]
+        with self.conn.cursor(dictionary=True) as cursor:
+            set_clause = [f'{key} = %s' for key in data]
 
-        query = f'UPDATE {self.table_name} SET ({set_clause}) WHERE id = %s'
-        params = list(data.values()) + [item_id]
+            query = f'UPDATE {self.table_name} SET ({set_clause}) WHERE id = %s'
+            params = list(data.values()) + [item_id]
 
-        cursor.execute(query, params)
-        self.conn.commit()
+            cursor.execute(query, params)
+            self.conn.commit()
 
-        changed = cursor.rowcount > 0
-        cursor.close()
-        return changed
+            changed = cursor.rowcount > 0
+            return changed
     
     def delete_item(self, item_id: int) -> bool:
-        cursor = self.conn.cursor()
-        query = f'DELETE FROM {self.table_name} WHERE id = %s'
-        
-        cursor.execute(query, (item_id,))
-        self.conn.commit()
+        with self.conn.cursor(dictionary=True) as cursor:
+            query = f'DELETE FROM {self.table_name} WHERE id = %s'
+            
+            cursor.execute(query, (item_id,))
+            self.conn.commit()
 
-        changed = cursor.rowcount > 0
-        cursor.close()
-        return changed
+            changed = cursor.rowcount > 0
+            return changed
