0b31a17a84d6c388b66b133e1ccf519085b747ce
Hallel <hallelkedar000@gmail.com>
2026-06-11 11:57:49 +0300

    Update DBconnection object

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,47 +1,44 @@
 import mysql.connector
 from secret import DB_PASSWORD
 
-def get_connect(host, user, password, database):
-    conn = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    
-    return conn
+class DBconnection:
+    def __init__(self, host, user, password, database):
+        self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.conn.cursor(dictionary=True)
 
+    def create_tables(self):
 
-def create_tables(conn):
-
-    cursor = conn.cursor()
-
-    cursor.execute('''
-                CREATE TABLE IF NOT EXISTS books (
-                    id INT AUTO_INCERMENT PRIMARY KEY,
-                    title VARCHAR(50) NOT NULL,
-                    author VARCHAR(50) NOT NULL,
-                    genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other'),
-                    is_available BOOLEAN NOT NULL,
-                    borrowed_by_member_id INT UNIQUE
+        self.cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS books (
+                        id INT AUTO_INCERMENT PRIMARY KEY,
+                        title VARCHAR(50) NOT NULL,
+                        author VARCHAR(50) NOT NULL,
+                        genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other'),
+                        is_available BOOLEAN NOT NULL,
+                        borrowed_by_member_id INT UNIQUE
+                        );
+                ''')
+        self.cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS members (
+                        id INT AUTO_INCERMENT PRIMARY KEY,
+                        name VARCHAR(50) NOT NULL,
+                        email VARCHAR(255) UNIQUE NOT NULL,
+                        is_active BOOLEAN NOT NULL,
+                        total_borrows INT AUTO_INCREMENT NOT NULL
                     );
-            ''')
-    cursor.execute('''
-                CREATE TABLE IF NOT EXISTS members (
-                    id INT AUTO_INCERMENT PRIMARY KEY,
-                    name VARCHAR(50) NOT NULL,
-                    email VARCHAR(255) UNIQUE NOT NULL,
-                    is_active BOOLEAN NOT NULL,
-                    total_borrows INT AUTO_INCREMENT NOT NULL
-                   );
-                   ''')
-    conn.commit()
+                    ''')
+        self.conn.commit()
 
-def close_db(cursor, conn):
-    cursor.close()
-    conn.close()
+    def close_db(self):
+        self.cursor.close()
+        self.conn.close()
 
-db_connection = get_connect(host='localhost',
+db_connection = DBconnection(host='localhost',
                     user='root', 
                     password=DB_PASSWORD,
                     database='library_db'
