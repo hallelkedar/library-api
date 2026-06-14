9cb93984068d5d23c27d47b5546391db6f1e5ac5
Hallel <hallelkedar000@gmail.com>
2026-06-14 12:28:52 +0300

    Added logger msgs

--- routes/member_route.py
+++ routes/member_route.py
@@ -2,36 +2,51 @@ from fastapi import APIRouter
 from routes.model import Member, UpdateMember
 from services.app_service import member_db
 from services import member_service
+from logs.logger import logger
 
 router = APIRouter()
 
 @router.get('')
 def get_all_members():
-    return member_db.get_all_members()
+    all_members = member_db.get_all_members()
+    logger.info('Return all members list')
+    return all_members
 
 @router.post('')
 def create_member(data: Member):
     member = data.model_dump()
     new_id = member_db.create_member(member)
-    return {'detail': f'Member (-id-:{new_id}) created'}
+
+    return_msg = {'detail': f'Member (-id-:{new_id}) created'}
+    logger.info(return_msg['detail'])
+    return return_msg
 
 @router.get('/{id}')
 def get_member(id: int):
-    return member_service.get_member(id)
+    member = member_service.get_member(id)
+    logger.info(f'Return member number - {id}')
+    return member
 
 @router.patch('/{id}')
 def update_member(id, data: UpdateMember):
     member_data = data.model_dump(exclude_unset=True)
     member_service.get_member(id)
     member_db.update_member(id, member_data)
-    return {'detail': f'Member (-id-:{id}) updated'}
+    
+    return_msg = {'detail': f'Member (-id-:{id}) updated'}
+    logger.info(return_msg['detail'])
+    return return_msg
 
 @router.patch('/{id}/deactivate')
 def deactivate_member(id: int):
     member_service.set_member_activity(id, False)
-    return {'detail': f'Member (-id-:{id}) is no longer active'}
+    return_msg = {'detail': f'Member (-id-:{id}) is no longer active'}
+    logger.info(return_msg['detail'])
+    return return_msg
 
 @router.patch('/{id}/activate')
 def activate_member(id: int):
     member_service.set_member_activity(id, True)
-    return {'detail': f'Member (-id-:{id}) is active again'}
+    return_msg = {'detail': f'Member (-id-:{id}) is active again'}
+    logger.info(return_msg['detail'])
+    return return_msg
