020823f22490522c00e4f039dc44c248225831e9
Hallel <hallelkedar000@gmail.com>
2026-06-14 12:31:09 +0300

    Added logger msgs

--- routes/report_route.py
+++ routes/report_route.py
@@ -1,17 +1,25 @@
 from fastapi import APIRouter
 from services.app_service import book_db, member_db
 from services import reports_service
+from logs.logger import logger
 
 router = APIRouter()
 
 @router.get('/summery')
 def get_report_summery():
-    return reports_service.summery_report()
+    summery = reports_service.summery_report()
+    logger.info('Return summery reports dict')
+    return summery
 
 @router.get('/books-by-genre')
 def get_books_by_genre():
-    return book_db.count_by_genre()
+    count_by_genre = reports_service.summery_report()
+    logger.info('Return count by genre dict')
+    return count_by_genre
+
 
 @router.get('/top-member')
 def get_top_member():
-    return member_db.get_top_member()
+    top_member = member_db.get_top_member()
+    logger.info('Return top member dict')
+    return top_member
