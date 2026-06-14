d37ec2057bd49290d5837f3ace96e7fe6902f49d
Hallel <hallelkedar000@gmail.com>
2026-06-14 15:13:32 +0300

    Validation for unique email

--- services/member_service.py
+++ services/member_service.py
@@ -1,6 +1,13 @@
 from fastapi import HTTPException
 from services.app_service import member_db
 
+
+def create_member(data: dict):
+    new_id = member_db.create_member(data)
+    if not new_id:
+        raise HTTPException(400, 'Email is used by another member')
+    return new_id
+
 def get_member(member_id: int) -> dict | None:
     member = member_db.get_member_by_id(member_id)
     if not member:
